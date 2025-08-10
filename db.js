0e90579b746176740402a043070584e7f64810d6
Emeline D.R. <deroo.emeline@gmail.com>
2025-08-10 14:35:45 +0200

    Forcer IPv4

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 import pg from "pg";
+import dns from "dns";
 import dotenv from "dotenv";
 
 dotenv.config();
 const { Pool } = pg;
+
+// Forcer la résolution IPv4
+dns.setDefaultResultOrder("ipv4first");
 
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
