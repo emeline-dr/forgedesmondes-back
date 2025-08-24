88ee4db723a7d260e4b230e8823792504ddddb25
Emeline D.R. <deroo.emeline@gmail.com>
2025-08-24 17:12:04 +0200

    Hashage du mdp

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,5 @@
 import pool from "../db.js";
+import bcrypt from "bcrypt";
 
 export async function getAllUsers() {
     const result = await pool.query("SELECT * FROM users");
@@ -10,11 +11,24 @@ export async function getUserById(id) {
     return result.rows[0];
 }
 
+export async function verifyUser(email, password) {
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [email]);
+    const user = result.rows[0];
+    if (!user) return false;
+
+    const match = await bcrypt.compare(password, user.password);
+    return match ? user : false;
+}
+
 export async function createUser(username, email, password) {
     const createdAt = new Date();
+
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
     const result = await pool.query(
         "INSERT INTO users (username, email, password, created_at) VALUES ($1, $2, $3, $4) RETURNING *",
-        [username, email, password, createdAt]
+        [username, email, hashedPassword, createdAt]
     );
     return result.rows[0];
 }
