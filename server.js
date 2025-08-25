cc7a2a2c6abc0c62e67ab87f9e3d264d26d28654
Emeline D.R. <deroo.emeline@gmail.com>
2025-08-25 11:19:40 +0200

    Modification CORS

--- server.js
+++ server.js
@@ -2,12 +2,30 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cookieParser());
 app.use(express.json());
+
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://forge-des-mondes.vercel.app/"
+];
+
+app.use(cors({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("Origine non autorisée"));
+        }
+    },
+    credentials: true,
+}));
 
 app.use("/users", userRoutes);
 
